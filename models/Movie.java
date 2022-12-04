cbfe4e90980b20d625eb6812ad5ca9aaf90d667b
chimaihueze <kihueze@gmail.com>
2022-12-04 07:48:55 +0100

    added fields to the Movie.java class

--- models/Movie.java
+++ models/Movie.java
@@ -1,5 +1,11 @@
 package models;
 
 public class Movie {
+    private String name;
+    private String format;
+    private double rating;
+    private double sellingPrice;
+    private double rentingPrice;
+    private String isAvailable;
     
 }
