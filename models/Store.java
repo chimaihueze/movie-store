0af4aed068ea6ae0ce37b1c188e5b72fd45d571a
Chima Ihueze <drkingsleyc@gmail.com>
2022-12-04 11:44:42 +0100

    added the Store constructor

--- models/Store.java
+++ models/Store.java
@@ -4,4 +4,9 @@ import java.util.ArrayList;
 
 public class Store {
     private ArrayList<Movie> movies;
+
+    // constructor
+    public Store() {
+        this.movies = new ArrayList<Movie>();
+    }
 }
