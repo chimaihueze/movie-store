c9a512b1d1b5b2522030efa70e9a463fad0668f3
Chima Ihueze <drkingsleyc@gmail.com>
2022-12-13 10:44:34 +0100

    closed the manageMovies() scanner

--- Main.java
+++ Main.java
@@ -20,20 +20,8 @@ public class Main {
         } finally {
             System.out.println("Process Complete\n\n");
         }
-
-
     }
 
-    /**
-     * Name: manageMovies
-     * Inside the function:
-     *   • 1. Starts a new instance of Scanner;
-     *   • 2. In an infinite loop, the user can choose to a) purchase b) rent c) return d) exit.
-     *   •        case a: ask for the name and sell.
-     *   •        case b: ask for the name and rent.
-     *   •        case c: ask for the name and return.
-     *   • 3. call close() from the Scanner object.
-     */
 
     public static void manageMovies() {
         Scanner scan = new Scanner(System.in);
@@ -71,20 +59,8 @@ public class Main {
             else {
                 break;
             }
-        }
+        } scan.close();
     }
-
-    /**
-     * Name: loadMovies
-     * @param fileName (String)
-     * @throws FileNotFoundException
-     *
-     * Inside the function:
-     *   • 1. loads movies from <fileName>.txt.
-     *   • 2. adds all movies to the store object's movie field.
-     *        Hint: You will need to 'split' a String into three Strings.
-     */
-
 
     public static void loadMovies(String fileName) throws FileNotFoundException {
         FileInputStream fis = new FileInputStream(fileName);
